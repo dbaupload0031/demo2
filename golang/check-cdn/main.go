f85ef03acd2bbc568d7bf8964f0044f2a1fa1711
SamChang <dba.upload0031@gmail.com>
2023-07-03 02:37:03 +0800

    Add files via upload

--- golang/check-cdn/main.go
+++ golang/check-cdn/main.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"check-cdn/mycloudflare"
+	"check-cdn/mytencent"
 	"fmt"
 	"log"
 	"os"
 )
 
 func main() {
+
+	cloudflare_token := os.Getenv("CLOUDFLARE_API_TOKEN")
+	Tencent_1_SecretId := os.Getenv("TENCENT_1_SECRETID")
+	Tencent_1_SecretKey := os.Getenv("TENCENT_1_SECRETKEY")
 
 	filename := "target.txt"
 	lines, err := mycloudflare.ReadFileToArray(filename)
@@ -18,15 +23,19 @@ func main() {
 
 	for _, purge_target := range lines {
 		//--------------------------------------------------------
-		cloudflare_token := os.Getenv("CLOUDFLARE_API_TOKEN")
+
 		clouflare_purge_result := mycloudflare.CloudflarePurgeResult(cloudflare_token, purge_target, "A")
 		fmt.Println("++ " + clouflare_purge_result + " ++")
 		if clouflare_purge_result == "DOMAIN_NOT_EXIST" {
 			fmt.Println("========> DOMAIN_NOT_EXIST pleae check again.")
 		} else if clouflare_purge_result == "SUBDOMAIN_NOT_EXIST" {
-			fmt.Println("========> go other cloud CDN check")
+			fmt.Println("========> go other cloud CDN check(" + purge_target + ")")
+			//tencent-1
+			if mytencent.PurgeCdn(Tencent_1_SecretId, Tencent_1_SecretKey, purge_target) {
+				continue
+			}
 			/*
-								if(tencent.PurgeCdn(tencent-1,x,x,c)) {continue}
+								if(mytencent.PurgeCdn(tencent-1,x,x,c)) {continue}
 								else if (tencent.PurgeCdn(tencent-2,x,x,c))
 								{continue}
 								else if (tencent.PurgeCdn(tencent-3,x,x,c))
